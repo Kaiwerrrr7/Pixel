1378155d69a04b01512768f8ba40a067fa5a1912
Kaiwerrrr7 <dungizukai008@gmail.com>
2024-10-05 23:40:15 +0700

    Add files via upload

---
+++ core/protocol/AirdropIds.py
@@ -0,0 +1,53 @@
+class AirdropIds:
+
+    BLUM = {
+        "site": "https://telegram.blum.codes/",
+        "short_name": "Blum",
+        "username": "BlumCryptoBot",
+        "app_web_view": False
+    }
+    OKX_RACER = {
+        "site": 'https://www.okx.com/',
+        "short_name": "OKX Racer",
+        "username": 'OKX_official_bot',
+        "app_web_view": False
+    }
+    CRYPTO_RANK = {
+        "site": "https://tma.cryptorank.io/",
+        "short_name": "CryptoRank",
+        "username": "CryptoRank_app_bot",
+        "app_web_view": False
+    }
+    TOMARKET = {
+        "site": "https://mini-app.tomarket.ai/",
+        "short_name": "Tomarket App",
+        "username": "Tomarket_ai_bot",
+        "app_web_view": False
+    }
+    THEYESCOIN = {
+        "site": "https://www.yescoin.gold/",
+        "short_name": "Yescoin",
+        "username": "theYescoin_bot",
+        "app_web_view": True
+    }
+    TIMEFARM = {
+        "site": "https://timefarm.app/",
+        "short_name": "Time Farm",
+        "username": "TimeFarmCryptoBot",
+        "app_web_view": False
+    }
+    BYBIT_COINSWEEPER = {
+        "site": "https://bybitcoinsweeper.com/",
+        "short_name": "Bybit Coinsweeper",
+        "username": "BybitCoinsweeper_Bot",
+        "app_web_view": False
+    }
+    NOT_PIXEL = {
+        "site": "https://image.notpx.app/",
+        "short_name": "Not Pixel",
+        "username": "notpixel",
+        "app_web_view": False
+    }
+
+    def __init__(self) -> None:
+        pass
